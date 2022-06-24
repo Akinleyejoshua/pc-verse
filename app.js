a17cd7e904659012aae7e48dc0bcc55b6fd3660c
Joshua <akinleyejoshua.dev@gmail.com>
2022-06-24 19:04:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,14 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(routes);
-app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:4200');
-    if(req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
-        return res.status(200).json({});
-    }
-    next();
-});
 app.use(cors());
 
 module.exports = app;
