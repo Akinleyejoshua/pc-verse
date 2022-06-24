407fe67868dc87a10d7cd19ffdea6e0690abac6c
Joshua <akinleyejoshua.dev@gmail.com>
2022-06-24 19:05:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
-
+app.use(cors());
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(routes);
-app.use(cors());
+
 
 module.exports = app;
